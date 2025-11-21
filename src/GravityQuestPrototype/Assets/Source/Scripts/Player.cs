8ee223501d62ffbb43d466ac598790d6810285ec
Nikolai Kochergin <Kocherginizer@gmail.com>
2025-11-21 12:32:26 +0300

    Добавил управление игроком.

---
+++ src/GravityQuestPrototype/Assets/Source/Scripts/Player.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Source.Scripts
+{
+    public class Player : MonoBehaviour
+    {
+        public void SetMoveInput(float value)
+        {
+            Debug.Log($"<color=orange> Move {value}");
+        }
+
+        public void RequestJump()
+        {
+            Debug.Log($"<color=green> RequestJump");
+        }
+    }
+}
---
+++ src/GravityQuestPrototype/Assets/Source/Scripts/PlayerInput.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Source.Scripts
+{
+    public class PlayerInput : MonoBehaviour
+    {
+        [SerializeField] private Player _player;
+        
+        private InputAction _moveAction;
+        private InputAction _jumpAction;
+
+        private void Awake()
+        {
+            _moveAction = InputSystem.actions.FindAction("Move");
+            _jumpAction = InputSystem.actions.FindAction("Jump");
+        }
+
+        private void OnEnable()
+        {
+            _moveAction.performed += OnMove;
+            _moveAction.canceled += OnMove;
+
+            _jumpAction.performed += OnJump;
+        }
+
+        private void OnMove(InputAction.CallbackContext ctx)
+        {
+            float value = ctx.ReadValue<float>();
+            _player.SetMoveInput(value);
+        }
+
+        private void OnJump(InputAction.CallbackContext ctx)
+        {
+            if (ctx.performed)
+                _player.RequestJump();
+        }
+    }
+}
