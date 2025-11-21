66f67d198dfdd9b694be175b34dab37e4337d991
Nikolai Kochergin <Kocherginizer@gmail.com>
2025-11-21 15:02:55 +0300

    Добавил возможность перепрыгивать на другие платформы.

---
+++ src/GravityQuestPrototype/Assets/Source/Scripts/Gameplay/Platforms/GravityFieldTrigger.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Source.Scripts.Gameplay
+{
+    public class GravityFieldTrigger : MonoBehaviour
+    {
+        [SerializeField] private Collider2D _platformCollider;
+        
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if(other.attachedRigidbody.TryGetComponent(out Player player))
+                player.SetPlatform(_platformCollider);
+        }
+    }
+}
